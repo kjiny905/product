a3cb59c47c105217869c62dc57c23a67f34a1d6c
pkj <kjiny905217@gmail.com>
2023-03-24 16:51:35 +0900

    소스_서버프로그램구현_박경진

--- product/src/main/java/com/kh/product/svc/ProductSVC.java
+++ product/src/main/java/com/kh/product/svc/ProductSVC.java
@@ -1,19 +1,19 @@
-package com.kh.product.dao;
+package com.kh.product.svc;
+
+import com.kh.product.dao.Product;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductDAO {
-  // 등록
+public interface ProductSVC {
+  //등록
   Long save(Product product);
-  // 조회
+  //조회
   Optional<Product> findById(Long pid);
-  // 수정
+  //수정
   int update(Long pid, Product product);
-  // 삭제
+  //삭제
   int delete(Long pid);
-
   //목록
   List<Product> findAll();
-
 }
